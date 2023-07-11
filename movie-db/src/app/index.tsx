65ac8eb48975f21dae96d0da2f4ef76f4eee135c
Brandon Hernandez <graybuho76@gmail.com>
2023-07-10 23:25:01 -0600

    initial build

---
+++ movie-db/src/app/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-centerpy-2">
+            <Head>
+                <title>MovieDB Brandon Hernandez</title>
+            </Head>
+
+        </div>
+    )
+}
---
+++ movie-db/src/app/page.tsx
@@ -0,0 +1,14 @@
+
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+
+    <div className="flex min-h-screen flex-col items-center justify-centerpy-2">
+      <Head>
+        <title>MovieDB Brandon Hernandez</title>
+      </Head>
+      <p>Welcome sir</p>
+    </div>
+  )
+}
